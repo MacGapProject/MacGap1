d87e9cf1db0bce66cbf2fcfca17a965beb575afd
Jeff Hanbury <jeff@marmaladesoul.com>
2014-03-08 13:11:30 +1300

    Add defaultLineHeightForFont

--- MacGap/Classes/Commands/fonts.h
+++ MacGap/Classes/Commands/fonts.h
@@ -4,5 +4,6 @@
 - (NSArray*) availableFonts;
 - (NSArray*) availableFontFamilies;
 - (NSArray*) availableMembersOfFontFamily:(NSString*)fontFamily;
+- (CGFloat)  defaultLineHeightForFont:(NSString *)theFontName ofSize:(CGFloat)theFontSize;
 
 @end
--- MacGap/Classes/Commands/fonts.m
+++ MacGap/Classes/Commands/fonts.m
@@ -12,32 +12,36 @@
 }
 
 - (NSArray*) availableMembersOfFontFamily:(NSString *)fontFamily {
-    //- (NSArray*) availableMembersOfFontFamily {
-    //    NSString *fontFamily = @"Helvetica";
     return [[NSFontManager sharedFontManager] availableMembersOfFontFamily:fontFamily];
+}
+
+- (CGFloat) defaultLineHeightForFont:(NSString*)theFontName ofSize:(CGFloat)theFontSize {
+    NSFont *theFont = [NSFont fontWithName:theFontName size:theFontSize];
+    NSLayoutManager *lm = [[NSLayoutManager alloc] init];
+    
+    return [lm defaultLineHeightForFont:theFont];
 }
 
 
 #pragma mark WebScripting Protocol
 
-+ (BOOL) isSelectorExcludedFromWebScript:(SEL)selector
-{
++ (BOOL) isSelectorExcludedFromWebScript:(SEL)selector {
     return NO;
 }
 
-+ (NSString*) webScriptNameForSelector:(SEL)selector
-{
++ (NSString*) webScriptNameForSelector:(SEL)selector {
 	id	result = nil;
 	
 	if (selector == @selector(availableMembersOfFontFamily:)) {
 		result = @"availableMembersOfFontFamily";
-	}
-	
+    } else if (selector == @selector(defaultLineHeightForFont:ofSize:)) {
+        result = @"defaultLineHeightForFont";
+    }
+
 	return result;
 }
 
-+ (BOOL) isKeyExcludedFromWebScript:(const char*)name
-{
++ (BOOL) isKeyExcludedFromWebScript:(const char*)name {
 	return NO;
 }
 
