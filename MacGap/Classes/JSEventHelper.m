7c95ed3f8db6bfca95208f9b3752760f22921e81
Jeff Hanbury <jeff@marmaladesoul.com>
2014-04-26 14:19:28 +1200

    Switch to using shared Utils convertDictionaryToJSON.

--- MacGap/Classes/JSEventHelper.m
+++ MacGap/Classes/JSEventHelper.m
@@ -21,19 +21,7 @@
 + (void) triggerEvent:(NSString *)event withArgs:(NSDictionary *)args forObject:(NSString *)objName forWebView:(WebView *)webView {
     
     // Convert args Dictionary to JSON.
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization
-                        dataWithJSONObject:args
-                        options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
-                        error:&error];
-    
-    NSString *jsonString;
-    if (! jsonData) {
-        NSLog(@"Got an error converting to JSON: %@", error);
-    }
-    else {
-        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    }
+    NSString* jsonString = [[Utils sharedInstance] convertDictionaryToJSON:args];
     
     // Create the event JavaScript and run it.
     NSString * str = [NSString stringWithFormat:@"var e = document.createEvent('Events'); e.initEvent('%@', true, false);  e.data=%@; %@.dispatchEvent(e); ", event, jsonString, objName];
